db868326aca48e5e7f2927cc293da84bc01afd1a
Nicolas Martinez <nicolas.martinez23@gmail.com>
2017-02-16 22:05:42 -0300

    add ng-click

--- js/libs/todo.controller.js
+++ js/libs/todo.controller.js
@@ -1,16 +1,23 @@
 function MoviesController() {
+  this.likesList = [];
+  this.unlike = function (index) {
+    this.likesList.splice(index, 1);
+  };
+  this.addToLikes = function (movie) {
+    this.likesList.push(movie);
+  };
   this.favorites = [{
-    title: 'Front',
+    title: 'Final Fantasy',
     year: 1994,
     popular: true,
     category: 'fantasy'
   },{
-    title: 'Termit',
+    title: 'Shrek',
     year: 2000,
     popular: false,
     category: 'fantasy'
   },{
-    title: 'Scare',
+    title: 'Terminator',
     year: 2010,
     popular: true,
     category: 'action'
